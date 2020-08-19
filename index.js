e1d693668ae48325e5db46cce94cfd254685d9a3
Matthew Hartman <matthewhartmanau@gmail.com>
2020-08-19 21:01:59 +1000

    Updating app and added packages for text formatting

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+#!/usr/bin/env node
+'use strict';
+
 const https = require('https');
 const cheerio = require('cheerio');
 const chalk = require('chalk');
-// https://stackoverflow.com/a/28641253
-const tab = require('tab');
 
-https.get('https://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCodes=ben+apt', (response) => {
+const yargs = require('yargs');
+const columnify = require('columnify');
+
+const options = yargs
+.default('c', 'CSL,CBA,BHP,WBC,NAB,FMG,WES,ANZ,WOW,MQG,RIO,TCL,TLS,GMG,NCM', 'Show all ASX codes')
+.usage('Usage: -a <code>')
+.option('a', { alias: 'all', describe: 'Show all ASX codes', type: 'string', demandOption: false })
+.usage('Usage: -c <code>')
+.option('c', { alias: 'code', describe: 'ASX code (eg: BEN or multiple codes BEN,CBA,ATP)', type: 'string', demandOption: true })
+.argv;
+
+const code = `${options.code}`.replace(/,/g, '+');
+console.clear();
+
+https.get(`https://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCodes=${code}`, (response) => {
   response.setEncoding('utf8');
   let rawData = '';
   response.on('data', (chunk) => { rawData += chunk; });
@@ -12,40 +27,43 @@ https.get('https://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCod
     try {
       const parsedData = rawData;
       const $ = cheerio.load(parsedData);
-      let table = new tab.TableOutputStream({
-        'columns': [
-          { label: 'code', align: 'left', width: 10 },
-          { label: 'last price', align: 'left', width: 10 },
-          { label: 'change $+/-', align: 'left', width: 10 },
-          { label: 'change %', align: 'left', width: 10 },
-          { label: 'bid price', align: 'left', width: 10 },
-          { label: 'offer price', align: 'left', width: 10 },
-          { label: 'open price', align: 'left', width: 10 },
-          { label: 'daily high', align: 'left', width: 10 },
-          { label: 'daily low', align: 'left', width: 10 },
-          { label: 'volume', align: 'left', width: 10 }
-        ],
-        'omitHeader': true
-      });
+      let results = []
       // regex - https://stackoverflow.com/a/33823632
       $('.datatable tr').each((index, element) => {
         const row = $(element).text().trim().replace('*', '').replace(/\s+/g, '| ');
-        const dataset = row.split('|');
-        table.writeRow([
-          dataset[0], // code
-          dataset[1], // last price
-          dataset[2], // change $+/-
-          dataset[3], // change %
-          dataset[4], // bid price
-          dataset[5], // offer price
-          dataset[6], // open price
-          dataset[7], // daily high
-          dataset[8], // daily low
-          dataset[9]  // volume
-        ]);
+        const column = row.split('| ');
+        const change = column[2].charAt(0);
+        if (index !== 0) {
+          results.push({
+            code: column[0],
+            last: column[1],
+            '$+/-': (change === '-' && index !== 0) ? chalk.red(column[2]) : chalk.green(column[2]),
+            '% chg': column[3],
+            bid: column[4],
+            offer: column[5],
+            open: column[6],
+            high: column[7],
+            low: column[8],
+            volume: column[9]
+          });
+        }
       });
+      const options = { truncate: true, config: {
+        code: { minWidth: 10 },
+        last: { minWidth: 10 },
+        '$+/-': { minWidth: 10 },
+        '% chg': { minWidth: 10 },
+        bid: { minWidth: 10 },
+        offer: { minWidth: 10 },
+        open: { minWidth: 10 },
+        high: { minWidth: 10 },
+        low: { minWidth: 10 },
+        volume: { minWidth: 10 },
+      }}
+      const columns = columnify(results, options)
+      console.log(columns)
     } catch (e) {
-      console.error(e.message);
+    console.error(e.message);
     }
   })
 })
